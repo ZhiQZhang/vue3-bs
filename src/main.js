b6b014a35fd0076d6d0e650655f9d0a8bbeb64fe
zzq <1731492139@qq.com>
2023-02-03 11:49:14 +0800

    完成vant组件及插件的注册和配置

--- src/main.js
+++ src/main.js
@@ -2,5 +2,8 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+// 引入vant和样式并注册vant组件
+import vant from 'vant'
+import 'vant/lib/index.css'
 
-createApp(App).use(store).use(router).mount('#app')
+createApp(App).use(store).use(router).use(vant).mount('#app')

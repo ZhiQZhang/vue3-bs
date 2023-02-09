934c804e3bac2adb58509850868ae962916f4bb3
zzq <1731492139@qq.com>
2023-02-09 20:26:42 +0800

    添加共用样式及导入头组件

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,16 @@
 <template>
-  <div>
-
-  </div>
+    <header-components></header-components>
+    <div class="container">
+    </div>
 </template>
 
 <script setup>
-
+import headerComponents from './components/header/headerComponents.vue'
 </script>
 
 <style lang="scss">
-
+.container{
+    width: 100%;
+    height: 100%;
+}
 </style>

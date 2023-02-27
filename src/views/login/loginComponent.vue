90a98c61bc9ac68e2a5a6364c6d284153fe57d15
zzq <1731492139@qq.com>
2023-02-27 22:06:29 +0800

    继续完善登录，完成input样式修改

--- src/views/login/loginComponent.vue
+++ src/views/login/loginComponent.vue
@@ -1,11 +1,13 @@
 <template>
     <div class="loginContainer">
+        <p>欢迎使用租房数据可视化平台</p>
         <div class="form">
-            <input v-model="usn" type="text" name="" id="usn">
-            <input v-model="pwd" type="text" name="" id="pwd">
+            <input v-model="usn" :class="[usn !== '' ? 'act' : '' ]" type="text" name="usn" id="usn">
+            <label for="usn" :class="[ usn !== '' ? 'active' : '' ]">用户名</label>
+            <input v-model="pwd" :class="[pwd !== '' ? 'act' : '' ]" type="text" name="pwd" id="pwd">
+            <label for="pwd" :class="[ pwd !== '' ? 'active' : '' ]">密码</label>
             <button @click="login">登录</button>
         </div>
-        <router-link to="/detail">详情</router-link>
     </div>
 </template>
 
@@ -39,21 +41,85 @@ onMounted(() => {
 .loginContainer{
     width: 100%;
     height: 100%;
+    background-color: #66B1FF;
+    position: relative;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    p{
+      height: 20%;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      color: #fff;
+      font-size: 22px;
+    }
     .form{
         margin: 0 auto;
         width: 60%;
         height: 50%;
-        display: flex;
-        flex-direction: column;
-        align-items: center;
+        position: relative;
+        text-align: center;
         input{
-            width: 100%;
-            height: 8%;
+            width: 80%;
+            height: 12%;
+            margin-bottom: 2%;
+            border-radius: 10px;
+            border: 1px solid transparent;
+            font-size: 18px;
+            transition: 0.2s all ease;
         }
         button{
-            width: 30%;
-            height: 10%;
+            width: 40%;
+            height: 13%;
+            border-radius: 10px;
+            border: 1px solid transparent;
+            font-size: 20px;
+            color: #808080;
+        }
+        label{
+          text-align: left;
+          position: absolute;
+          height: 0;
+          width: 60px;
+          transform: translate(-675px, 8px);
+          color: #808080;
+          font-size: 18px;
+          height: 10px;
+        }
+        input:focus{
+          border: 3px solid red;
+        }
+        input:focus + label{
+          text-align: center;
+          font-size: 14px;
+          color: red;
+          transform: translate(-675px, -7px);
+          background-color: #66B1FF;
+        }
+        label.active{
+          text-align: center;
+          font-size: 14px;
+          color: red;
+          transform: translate(-675px, -7px);
+          background-color: #66B1FF;
+        }
+        input.act{
+          border: 3px solid red;
         }
     }
+    // span.usn{
+    //   position: absolute;
+    //   transform: translate(300px, 115px);
+    //   color: #808080;
+    //   height: 0;
+    // }
+    // span.pwd{
+    //   position: absolute;
+    //   transform: translate(300px, 160px);
+    //   color: #808080;
+    //   height: 0;
+    // }
 }
 </style>
